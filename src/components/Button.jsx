50a7057a508333e712404f42f0166070ad3e69ff
Ryan Brito <rm554497@fiap.com.br>
2024-09-24 18:39:41 -0300

    Corrigindo Botão

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = (props) => {
     return (
       <a
         href="https://github.com/ryanbritodev/portfolio-react"
-        className="bg-black flex md:bg-fiap text-white font-montserrat font-semibold py-2 px-6 w-36 rounded md:ml-8 
+        className="bg-black flex items-center md:bg-fiap text-white font-montserrat font-semibold py-2 px-7 w-36 rounded md:ml-8 
                    hover:bg-[#b3184a] duration-500 ease-in-out
                    active:bg-[#8a1138] active:text-white focus:bg-[#b3184a] focus:ring-4 focus:ring-[#e4336c] focus:outline-none"
       >
