9eeab5781d49c3c18eb41f75218c887879df2f9b
Ryan Brito <rm554497@fiap.com.br>
2024-10-01 08:59:08 -0300

    Cor Select e Input

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -30,7 +30,7 @@ const Projects = ({ projects, projectImages }) => {
             maxLength={20}
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="font-montserrat px-4 py-2 w-full text-white rounded-lg border-2 border-white focus:outline-none focus:ring-2 focus:ring-fiap"
+            className="font-montserrat px-4 py-2 w-full bg-black text-white rounded-lg border-2 border-white focus:outline-none focus:ring-2 focus:ring-fiap"
           />
           <FaSearch className="absolute right-3 top-3 text-white" />
         </div>
@@ -39,7 +39,7 @@ const Projects = ({ projects, projectImages }) => {
         <select
           value={selectedCategory}
           onChange={(e) => setSelectedCategory(e.target.value)}
-          className="px-4 font-montserrat py-2 w-full md:w-auto text-white rounded-lg border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-fiap "
+          className="px-4 font-montserrat py-2 w-full bg-black md:w-auto text-white rounded-lg border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-fiap "
         >
           <option value="Todos os Projetos">Todos os Projetos</option>
           <option value="Front-end">Front-end</option>
