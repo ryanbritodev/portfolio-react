92867fc9e7840be96725ea2df165ea6274ca5333
Ryan Brito <rm554497@fiap.com.br>
2025-03-22 21:55:28 -0300

    🧡 add: Orange Preloader

--- pages/App.jsx
+++ pages/App.jsx
@@ -1,20 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Footer from '../src/components/Footer';
 import Nav from '../src/components/Nav';
 import Particle from '../src/components/Particle';
 import { Outlet } from 'react-router-dom';
 import ScrollToTop from '../src/components/ScrollToTop';
+import Preloader from '../src/components/Preloader/Preloader.jsx';
 import '../src/index.css';
 
 function App() {
+  const [showPreloader, setShowPreloader] = useState(true);
+  
+  const handleLoadingFinish = () => {
+    setShowPreloader(false);
+  };
+  
   return (
     <div>
+      {showPreloader && <Preloader onFinish={handleLoadingFinish} />}
       <ScrollToTop />
       <Particle />
       <Nav />
       <Outlet />
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import Logo from '../../assets/svg/Logo.svg';
+
+const Preloader = ({ onFinish }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onFinish();
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [onFinish]);
+
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center z-[9999]"
+      style={{ backgroundColor: '#74320b' }}
+    >
+      <img src={Logo} alt="Logo" className="w-32 h-32 animate-spin" />
+    </div>
+  );
+};
+
+export default Preloader;
